d3ac7334314ca910b652d968654317af872a4905
Théo DRAPPIER <theo.drappier85@gmail.com>
2017-12-12 19:17:24 +0100

    Add verification blank textarea

--- web/lib/script.js
+++ web/lib/script.js
@@ -1,8 +1,13 @@
 function send() {
   var value = document.getElementById('task').value;
-  var xhttp = new XMLHttpRequest();
-  xhttp.open('POST', '../app/insertTask.php', true);
-  xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhttp.send('task=' + value);
-  window.location.href = '.';
+  var testValue = value.replace(' ', '');
+  if (testValue === '') {
+    document.getElementById('error').classList.remove('hidden');
+  } else {
+    var xhttp = new XMLHttpRequest();
+    xhttp.open('POST', '../app/insertTask.php', true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send('task=' + value);
+    window.location.href = '.';
+  }
 }

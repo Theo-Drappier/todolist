abc10804e5ec43500b0c3484e1355aea2053ea7e
Théo DRAPPIER <theo.drappier85@gmail.com>
2018-01-09 11:43:07 +0100

    Delete redirection when update a row

--- web/lib/script.js
+++ web/lib/script.js
@@ -14,21 +14,35 @@ function addRow() {
 
 function deleteRow(element) {
   var id = parseInt(element.id.replace('delete', ''));
+  parentElement = element.parentNode.parentNode;
 
   var xhttp = new XMLHttpRequest();
   xhttp.open('DELETE', '../app/deleteTask.php?idTask=' + id, false);
   //xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhttp.send(null);
-  window.location.reload();
+  parentElement.parentNode.removeChild(parentElement);
 }
 
 function updateRow(element) {
   var id = parseInt(element.id.replace('change', ''));
-  var state = document.getElementById('state' + id).value;
+  var state = parseInt(document.getElementById('state' + id).value);
 
   var xhttp = new XMLHttpRequest();
   xhttp.open('POST', '../app/updateTask.php', false);
   xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
   xhttp.send('idTask=' + id + '&state=' + state);
-  window.location.reload();
+  modifyStateHTML(id, state);
+}
+
+function modifyStateHTML(id, state) {
+  var nameTask = document.getElementById(id);
+  var nameText = nameTask.textContent;
+
+  if (state === 0) {
+    document.getElementById('state' + id).value = 1;
+    nameTask.innerHTML = "<strike>" + nameText + "</strike>";
+  } else if(state === 1) {
+    document.getElementById('state' + id).value = 0;
+    nameTask.innerHTML = nameText;
+  }
 }
